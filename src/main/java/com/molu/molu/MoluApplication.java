44ef4b9443697369da4f883e4e432ec3a45b2d5e
juno-choi <ililil9482@naver.com>
2022-11-17 22:56:06 +0900

    poject start

---
+++ src/main/java/com/molu/molu/MoluApplication.java
@@ -0,0 +1,13 @@
+package com.molu.molu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoluApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoluApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/molu/molu/MoluApplicationTests.java
@@ -0,0 +1,13 @@
+package com.molu.molu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoluApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
