b490640a1a2cefc7fafc2993e1b2f5ce41f8c16b
juno-choi <ililil9482@naver.com>
2022-11-18 10:00:13 +0900

    [기타] Naming BACK 목록 추가

--- src/main/java/com/molu/molu/common/Naming.java
+++ src/main/java/com/molu/molu/common/Naming.java
@@ -2,7 +2,7 @@ package com.molu.molu.common;
 
 public class Naming {
     private static final String[] FRONT = {"어쩔","저쩔","참나","안물","안궁","우짤","뇌절","저짤"};
-    private static final String[] BACK = {"빌드에러", "접속에러", "404", "500", "커밋오류", "깃충돌", "유효성검사"};
+    private static final String[] BACK = {"빌드에러", "접속에러", "404", "500", "커밋오류", "깃충돌", "유효성 검사", "파라미터 없음"};
 
     public static String getName(){
         int frontLength = FRONT.length;
