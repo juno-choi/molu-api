0121d0bd8dd1bc613cc9c8253d90e5b36a0a0720
juno-choi <ililil9482@naver.com>
2022-11-27 16:58:38 +0900

    [추가] Comment Controller 추가 및 문서화

---
+++ src/main/java/com/molu/molu/controller/board/CommentController.java
@@ -0,0 +1,36 @@
+package com.molu.molu.controller.board;
+
+import com.molu.molu.domain.api.CommonApi;
+import com.molu.molu.domain.dto.board.PostComment;
+import com.molu.molu.domain.enums.api.ResultCode;
+import com.molu.molu.domain.enums.api.ResultType;
+import com.molu.molu.domain.vo.board.PostCommentResponse;
+import com.molu.molu.service.board.CommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/comment")
+@RequiredArgsConstructor
+public class CommentController {
+    private final CommentService commentService;
+
+    @PostMapping("")
+    public ResponseEntity<CommonApi<PostCommentResponse>> postComment(@RequestBody PostComment postComment, BindingResult bindingResult){
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(
+                        CommonApi.<PostCommentResponse>builder()
+                                .resultCode(ResultCode.SUCCESS)
+                                .resultType(ResultType.NONE)
+                                .data(commentService.postComment(postComment))
+                                .build()
+                );
+    }
+}
--- src/main/java/com/molu/molu/domain/dto/board/PostComment.java
+++ src/main/java/com/molu/molu/domain/dto/board/PostComment.java
@@ -7,12 +7,16 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.NotEmpty;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class PostComment {
+    @NotEmpty(message = "게시판 번호는 필수값 입니다.")
     private Long boardId;
+    @NotEmpty(message = "댓글 내용은 비어있을 수 없습니다.")
     private String comment;
 }
